8dfb0a92714f3a58b25a2786159595bc63d9ddb4
Rose Wilson <rosewilson812@yahoo.com>
2021-01-17 20:14:17 -0600

    Added [Required] and [StringLength] to Name and Description. Added ContactEmail.

--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodingEventsDemo.ViewModels
 {
     public class AddEventViewModel
     {
+        [Required(ErrorMessage="Name is required.")]
+        [StringLength(50,MinimumLength =3, ErrorMessage ="Name must be 3-50 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description must be under 500 characters.")]
         public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
     }
 }

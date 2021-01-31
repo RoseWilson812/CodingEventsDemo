499b837565356ad35e249a794709cf01196c3f38
Rose Wilson <rosewilson812@yahoo.com>
2021-01-31 15:09:25 -0600

    Added EventCategory in Controllers, Data, Models, ViewModels, Views. Added Views/Create and Index. Added links to Shared/_Layout. Created EventCategory SQL table.

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,60 @@
+﻿using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> eventCategories = context.EventCategories.ToList();
+            ViewBag.title = "Event Category List";            
+            return View(eventCategories);
+        }
+        [HttpGet, Route("/EventCategory/Create")]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Category/Add")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name,
+                    
+                };
+
+                context.EventCategories.Add(newEventCategory);
+                context.SaveChanges();
+
+                return Redirect("/EventCategory/Create");
+            }
+
+            return View(addEventCategoryViewModel);
+        }
+
+   
+    }
+}
+
+
+

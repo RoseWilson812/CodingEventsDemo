de7c5338a1d12f0a1133eeafe3f2a7282eecd90e
Rose Wilson <rosewilson812@yahoo.com>
2021-02-11 22:13:50 -0600

    Didn't change anything(added a / in a comment) and it crashes in Register.cshtml.cs

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -8,7 +8,7 @@ using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+// /For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace coding_events_practice.Controllers
 {
